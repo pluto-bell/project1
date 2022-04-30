6386c3496abfb1059ece66c854f2d45cf38eec03
pluto-bell <pluto.belli@gmail.com>
2022-04-30 16:20:41 -0700

    Updates

---
+++ precache-manifest.f121f89d1f57a9aae929c4beb749510f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dcb1512ed9980ede296",
+    "url": "/project1/static/css/main.10ca4a96.chunk.css"
+  },
+  {
+    "revision": "6dcb1512ed9980ede296",
+    "url": "/project1/static/js/main.c6f8d0cb.chunk.js"
+  },
+  {
+    "revision": "d6fd7685404c86cd4e4c",
+    "url": "/project1/static/js/runtime~main.83c8361f.js"
+  },
+  {
+    "revision": "812dff5b8f2f748c3098",
+    "url": "/project1/static/js/2.119e925b.chunk.js"
+  },
+  {
+    "revision": "772ffc59d305818b48061d58383efa54",
+    "url": "/project1/static/media/project1-designSmile.772ffc59.svg"
+  },
+  {
+    "revision": "f030f5f684e1a22e21e1b7200d6495f5",
+    "url": "/project1/static/media/project1-contactCardsEMAIL.f030f5f6.svg"
+  },
+  {
+    "revision": "562ee90cdc1f9ee8010cfb1e0f8da2d1",
+    "url": "/project1/static/media/project1-contactCardsINSTA.562ee90c.svg"
+  },
+  {
+    "revision": "f1d2653fd022e8768378049d75184b9c",
+    "url": "/project1/static/media/project1-contactCardsETC.f1d2653f.svg"
+  },
+  {
+    "revision": "01896a6c2bf079d17dc9d35fbc8ff236",
+    "url": "/project1/static/media/projecr1-ilovebut.01896a6c.svg"
+  },
+  {
+    "revision": "ea301eb920546243b95cd9a4af3423fd",
+    "url": "/project1/static/media/project1-tobecome.ea301eb9.svg"
+  },
+  {
+    "revision": "bccb13cee81dab7bd482bf397b21e391",
+    "url": "/project1/static/media/project1-FEexcitesMe.bccb13ce.svg"
+  },
+  {
+    "revision": "1b1ca02a919d6abd59e4f25d48b8a60d",
+    "url": "/project1/static/media/project1-goodtaste.1b1ca02a.svg"
+  },
+  {
+    "revision": "249cf3bad4098f0c12b8728838364060",
+    "url": "/project1/static/media/project1-anartist.249cf3ba.svg"
+  },
+  {
+    "revision": "b3100572f1c058bf8bc3f2b87313325e",
+    "url": "/project1/static/media/project1-butlove.b3100572.svg"
+  },
+  {
+    "revision": "ef12bd1a1fc1660b06b1526fe14cd0f3",
+    "url": "/project1/static/media/project1-ihate.ef12bd1a.svg"
+  },
+  {
+    "revision": "7f0a7e92ba3c4458308a79faafb57948",
+    "url": "/project1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c101e758526059eb09a05fbc4d3c898.js"
+  "/project1/precache-manifest.f121f89d1f57a9aae929c4beb749510f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/project1/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
